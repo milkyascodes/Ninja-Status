58ca0ae3d18b086020d7dffdf89c095eaa95a2e1
miki <milkyas.dev@gmail.com>
2022-02-23 16:57:26 +0300

    added ninja_info

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -26,9 +28,81 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Ninja Status'),
-      ),
-    );
+        appBar: AppBar(
+          title: const Text('Ninja Status'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              SizedBox(
+                height: 430,
+                width: 320,
+                child: Card(
+                  elevation: 6,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      SizedBox(),
+                      Column(
+                        children: [
+                          CircleAvatar(
+                            radius: 60,
+                            backgroundImage:
+                                AssetImage('assets/ninja-titulek.png'),
+                          ),
+                          SizedBox(
+                            height: 30,
+                          ),
+                          Text(
+                            'Titulek',
+                            style: TextStyle(fontSize: 30),
+                          )
+                        ],
+                      ),
+                      SizedBox(
+                        child: Column(
+                          children: [
+                            Text(
+                              'Status',
+                              style: TextStyle(fontSize: 15),
+                            ),
+                            Text(
+                              'Chunin',
+                              style:
+                                  TextStyle(color: Colors.orange, fontSize: 25),
+                            )
+                          ],
+                        ),
+                      ),
+                      SizedBox(
+                        child: Column(
+                          children: [
+                            Text(
+                              'Level',
+                              style: TextStyle(fontSize: 15),
+                            ),
+                            Text(
+                              '🔥 🔥',
+                              style:
+                                  TextStyle(color: Colors.orange, fontSize: 15),
+                            )
+                          ],
+                        ),
+                      ),
+                      SizedBox()
+                    ],
+                  ),
+                ),
+              ),
+              IconButton(
+                onPressed: () {},
+
+                icon: Icon(Icons.add),
+                // color: Colors.red,
+              )
+            ],
+          ),
+        ));
   }
 }
